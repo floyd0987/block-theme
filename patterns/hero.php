dbb4fd0ca1118614b358b8ed8eaed8d968702c67
Eronne Bernucci <eronne.bernucci@immedia.net>
2024-12-15 18:12:52 +0100

    my-block

---
+++ patterns/hero.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Title: Hero
+ * Slug: block-theme/hero
+ * Description: Hero
+ * Viewport Width: 1024
+ * Categories: featured
+ * Keywords:
+ * Block Types: core/group
+ * Inserter: yes
+ *
+ * @package BlockTheme
+ */
+
+?>
+
+
+<!-- wp:group {"metadata":{"name":"Hero"},"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}},"position":{"type":""}},"backgroundColor":"black","layout":{"type":"default"}} -->
+<div
+  class="wp-block-group has-black-background-color has-background"
+  style="
+    padding-top: 0px;
+    padding-right: 0px;
+    padding-bottom: 0px;
+    padding-left: 0px;
+  "
+>
+  <!-- wp:cover {"url":"http://archimedestrutturemetalliche.local/wp-content/uploads/2024/12/239341950_4094064100692569_7433970468122201346_n.jpg","hasParallax":true,"dimRatio":0,"customOverlayColor":"#636662","isUserOverlayColor":false,"minHeight":563,"contentPosition":"center center","style":{"spacing":{"padding":{"bottom":"180px"}}},"layout":{"type":"default"}} -->
+  <div
+    class="wp-block-cover has-parallax"
+    style="padding-bottom: 180px; min-height: 563px"
+  >
+    <span
+      aria-hidden="true"
+      class="wp-block-cover__background has-background-dim-0 has-background-dim"
+      style="background-color: #636662"
+    ></span>
+    <div
+      class="wp-block-cover__image-background has-parallax"
+      style="
+        background-position: 50% 50%;
+        background-image: url(http://archimedestrutturemetalliche.local/wp-content/uploads/2024/12/239341950_4094064100692569_7433970468122201346_n.jpg);
+      "
+    ></div>
+    <div class="wp-block-cover__inner-container">
+      <!-- wp:group {"style":{"spacing":{"padding":{"top":"200px"}}},"layout":{"wideSize":"1170px","contentSize":"1170px","type":"constrained"}} -->
+      <div class="wp-block-group" style="padding-top: 200px">
+        <!-- wp:columns -->
+        <div class="wp-block-columns">
+          <!-- wp:column {"width":"700px"} -->
+          <div class="wp-block-column" style="flex-basis: 700px">
+            <!-- wp:heading {"level":1,"className":"zeever-animate zeever-move-right zeever-delay-1","style":{"typography":{"textTransform":"capitalize","fontStyle":"normal","fontWeight":"500","lineHeight":"1.2"},"spacing":{"margin":{"right":"0px","bottom":"40px"}}},"textColor":"zeever-secondary","fontSize":"tiny"} -->
+            <h1
+              class="wp-block-heading zeever-animate zeever-move-right zeever-delay-1 has-zeever-secondary-color has-text-color has-tiny-font-size"
+              style="
+                margin-right: 0px;
+                margin-bottom: 40px;
+                font-style: normal;
+                font-weight: 500;
+                line-height: 1.2;
+                text-transform: capitalize;
+              "
+            >
+              CREATIVE MIND, CREATIVE WORKS.
+            </h1>
+            <!-- /wp:heading -->
+
+            <!-- wp:heading {"level":1,"className":"zeever-animate zeever-move-right zeever-delay-3","style":{"typography":{"textTransform":"capitalize","fontStyle":"normal","fontWeight":"700","lineHeight":"1.2"},"spacing":{"margin":{"right":"0px","top":"23px"}}},"textColor":"zeever-primary","fontSize":"heading-1"} -->
+            <h1
+              class="wp-block-heading zeever-animate zeever-move-right zeever-delay-3 has-zeever-primary-color has-text-color has-heading-1-font-size"
+              style="
+                margin-top: 23px;
+                margin-right: 0px;
+                font-style: normal;
+                font-weight: 700;
+                line-height: 1.2;
+                text-transform: capitalize;
+              "
+            >
+              We Are Digital Agency
+            </h1>
+            <!-- /wp:heading -->
+
+            <!-- wp:buttons {"style":{"spacing":{"margin":{"top":"60px"}}}} -->
+            <div class="wp-block-buttons" style="margin-top: 60px">
+              <!-- wp:button {"className":"is-style-custombuttonfill zeever-animate zeever-animate zeever-move-right zeever-delay-1-move-right zeever-animate zeever-move-right zeever-delay-1-delay-5 is-style-custombuttonborder3","style":{"spacing":{"padding":{"top":"18px","bottom":"18px","left":"40px","right":"40px"}},"border":{"radius":"0px"}},"fontFamily":"helvetica-arial"} -->
+              <div
+                class="wp-block-button is-style-custombuttonfill zeever-animate zeever-move-right zeever-delay-1-move-right zeever-delay-1-delay-5 is-style-custombuttonborder3 has-helvetica-arial-font-family"
+              >
+                <a
+                  class="wp-block-button__link wp-element-button"
+                  href="#"
+                  style="
+                    border-radius: 0px;
+                    padding-top: 18px;
+                    padding-right: 40px;
+                    padding-bottom: 18px;
+                    padding-left: 40px;
+                  "
+                  >Getting Started</a
+                >
+              </div>
+              <!-- /wp:button -->
+            </div>
+            <!-- /wp:buttons -->
+          </div>
+          <!-- /wp:column -->
+
+          <!-- wp:column -->
+          <div class="wp-block-column"></div>
+          <!-- /wp:column -->
+        </div>
+        <!-- /wp:columns -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+  </div>
+  <!-- /wp:cover -->
+</div>
+<!-- /wp:group -->
