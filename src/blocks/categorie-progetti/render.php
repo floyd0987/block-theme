2f28de3d7cfc8a70a93d7805cf7ee05b9a54e91f
Eronne Bernucci <eronneb@gmail.com>
2025-11-29 20:57:06 +0100

    0.0.18

--- src/blocks/categorie-progetti/render.php
+++ src/blocks/categorie-progetti/render.php
@@ -7,7 +7,7 @@
 //  get_block_wrapper_attributes()
 
 
-if (!is_post_type_archive('progetti') && !is_tax('categorie-progetti')) {
+if (!is_post_type_archive('progetti')) {
 	return '';
 }
 
@@ -18,26 +18,39 @@ $terms = get_terms(array(
 
 
 
-if (empty($terms)) {
-	return '<p>No categories found.</p>';
-}
 ?>
-<div class="wp-block-block-theme-categorie-progetti categorie-progetti">
-	<ul class="categorie-progetti__list">
+<style>
 
-		<li class="categorie-progetti__item">
-			<a href="<?php echo esc_url(get_post_type_archive_link('progetti')); ?>" class="categorie-progetti__link">Tutti</a>
-		</li>
-
-		<?php foreach ($terms as $term) : ?>
-			<li class="categorie-progetti__item">
-				<a href="<?php echo esc_url(get_term_link($term)); ?>" class="categorie-progetti__link">
-					<?php echo esc_html($term->name); ?>
-				</a>
-			</li>
-		<?php endforeach; ?>
+</style>
+<?php
+if (empty($terms)) {
+    return '<p>No categories found.</p>';
+}
+// echo "<pre>";
+// print_r($terms);
+// echo "</pre>";
+?>
 
 
-	</ul>
+<div class="wp-block-block-theme-categorie-progetti">
+<div class="wp-block-query alignwide is-layout-flow wp-block-query-is-layout-flow">
+    <ul class="alignwide wp-block-post-template is-layout-grid categorie-progetti__grid">
+        <?php foreach ($terms as $term) : ?>
+            <?php $image = get_field('immagine', 'categorie-progetti_' . $term->term_id); ?>
+            <li class="categorie-progetti__item categorie-progetti-<?php echo esc_attr($term->slug); ?>">
+                <div class="wp-block-group alignwide has-border-color has-zeever-border-border-color is-layout-constrained" style="border-width:1px">
+                    <a href="<?php echo esc_url(get_term_link($term)); ?>" class="categorie-progetti__link wp-block-post-title has-normal-font-size">
 
+					<figure style="aspect-ratio:4/3;width:100%;" class="alignwide categorie-progetti__image-wrapper">
+                            <?php if ($image) : ?>
+                                <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($term->name); ?>" class="categorie-progetti__image" style="border-radius:0px;width:100%;height:100%;object-fit:cover;">
+                            <?php endif; ?>
+                        </figure>
+                        <div class="categorie-progetti__name"><?php echo esc_html($term->name); ?></div>
+                    </a>
+                </div>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</div>
 </div>
